7daf18547f514ba3a1ddb2b9533c530d81e24f30
James Lotruglio <jimlotruglio@yahoo.com>
2017-11-14 06:02:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,14 +31,7 @@ def move(board, input, token="X")
   board[input]= token
 end
 
- turn(board)
-  puts "Please enter 1-9:"
-  index = gets.strip
-  input_to_index(index)
-  if valid_move?(board, index)
-    move(board, index, "X")
-  end
-end
+ 
 
 def turn(board)
  puts "Please enter 1-9:"
